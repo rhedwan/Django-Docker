5e76df689280067769ca1ad480b3f0134dda347c
rhedwan <adesolaridwan2003@gmail.com>
2022-04-27 20:04:35 +0100

    Added Models

--- books/models.py
+++ books/models.py
@@ -4,3 +4,12 @@ from django.contrib.auth.models import AbstractUser
 
 class BooksCutomUser(AbstractUser):
     pass
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) :
+        return self.title
